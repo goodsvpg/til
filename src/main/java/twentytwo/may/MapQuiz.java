c23611473c8d4a1545da49d2f2bd180cb65dfa58
goodsvpg <goodbeprose26@gmail.com>
2022-05-06 16:54:03 +0900

    map quiz

---
+++ src/main/java/twentytwo/may/MapQuiz.java
@@ -0,0 +1,29 @@
+package twentytwo.may;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MapQuiz {
+    public List<Integer> square(List<Integer> numbers) {
+        return numbers.stream()
+                .map(number -> number * number)
+                .collect(Collectors.toList());
+    }
+
+    public List<Integer[]> pair(List<Integer> oneNumbers, List<Integer> twoNumbers) {
+        return oneNumbers.stream()
+                .flatMap(
+                        oneNumber -> twoNumbers.stream()
+                                .map(twoNumber -> new Integer[]{oneNumber,twoNumber})
+                ).collect(Collectors.toList());
+    }
+
+    public List<Integer[]> sumDivideResultZeroPair(int divided, List<Integer> oneNumbers, List<Integer> twoNumbers) {
+        return oneNumbers.stream()
+                .flatMap(
+                        oneNumber -> twoNumbers.stream()
+                                .filter(twoNumber -> (oneNumber + twoNumber) % divided == 0)
+                                .map(twoNumber -> new Integer[]{oneNumber, twoNumber})
+                ).collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/twentytwo/may/MapQuizTest.java
@@ -0,0 +1,62 @@
+package twentytwo.may;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MapQuizTest {
+    MapQuiz mapQuiz = new MapQuiz();
+    @Test
+    void square() {
+        //given
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        //when
+        List<Integer> square = mapQuiz.square(numbers);
+        //then
+        assertThat(square).isEqualTo(Arrays.asList(1, 4, 9, 16, 25));
+    }
+
+    @Test
+    void pair() {
+        //given
+        List<Integer> oneNumbers = Arrays.asList(1, 2, 3);
+        List<Integer> twoNumbers = Arrays.asList(3, 4);
+
+        //when
+        List<Integer[]> pair = mapQuiz.pair(oneNumbers, twoNumbers);
+
+        //then
+        assertThat(pair).hasSameElementsAs(
+                Arrays.asList(
+                        new Integer[]{1, 3}
+                        ,new Integer[]{2, 3}
+                        ,new Integer[]{3, 3}
+                        ,new Integer[]{1, 4}
+                        ,new Integer[]{2, 4}
+                        ,new Integer[]{3, 4}
+                )
+        );
+    }
+
+    @Test
+    void sumDivideThreeZeroPair() {
+        //given
+        List<Integer> oneNumbers = Arrays.asList(1, 2, 3);
+        List<Integer> twoNumbers = Arrays.asList(3, 4);
+
+        //when
+        List<Integer[]> pair = mapQuiz.sumDivideResultZeroPair(3, oneNumbers, twoNumbers);
+
+        //then
+        assertThat(pair).hasSameElementsAs(
+                Arrays.asList(
+                        new Integer[]{3, 3}
+                        ,new Integer[]{2, 4}
+                )
+        );
+    }
+}
